bc909fd54d75df56b8a0c4d717a2476237d5d3aa
midhunjac <46477063+midhunjac@users.noreply.github.com>
2023-02-20 15:40:39 +0000

    Update EMG_sensor.cpp

--- src/EMGSensor/EMG_sensor.cpp
+++ src/EMGSensor/EMG_sensor.cpp
@@ -1,17 +1,8 @@
 /*
-Adapted from https://github.com/Seeed-Studio/grove.py for C++ as part of the Physionix project
-Original Licence:
-The MIT License (MIT)
-Grove 8 channels 12 bit ADC Hat for the Raspberry Pi, used to connect grove sensors.
-Copyright (C) 2018  Seeed Technology Co.,Ltd.
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+Adapted from https://github.com/Seeed-Studio/grove.py/grove/adc_8chan_12bit.py for C++ as part of the Physionix project
+Author : Midhun Mathew
+Original date : 17/02/2023
+
 */
 
 #include "EMG_sensor.h"
