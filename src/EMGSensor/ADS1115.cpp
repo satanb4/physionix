1f2f98843b229817022ef66c1ece893413ae2f0b
Midhun <midhunjac2423@gmail.com>
2023-04-07 03:24:44 +0530

    Committing driver for ADS1115 and sample app
    Implemented callbacks for the ADS1115 ADC. Interrupts tested with ALRT pin of ADS1115 connected to GPIO 27 of Pi. values obtained are precise. Need to check the accuracy with leads attached.
    one has to implement the virtual func newdata() to get the new values.

--- src/EMGSensor/ADS1115.cpp
+++ src/EMGSensor/ADS1115.cpp
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <iostream>
 
-int ADS1115::i2c_write(ADS1115_device device, uint8_t reg, uint16_t data)
+#define START 1
+
+int ADS1115::_i2c_write(ADS1115_device device, uint8_t reg, uint16_t data)
 {
 	int ret = 0;
 	int fd = i2cOpen(I2C_BUS, device.addr, 0);
@@ -23,7 +25,7 @@ int ADS1115::i2c_write(ADS1115_device device, uint8_t reg, uint16_t data)
 	return ret;
 }
 
-int ADS1115::i2c_read(ADS1115_device device, uint8_t reg,uint16_t* data)
+int ADS1115::_i2c_read(ADS1115_device device, uint8_t reg,uint16_t* data)
 {
 	int ret = 0;
 	int fd = i2cOpen(I2C_BUS, device.addr, 0);
@@ -32,7 +34,7 @@ int ADS1115::i2c_read(ADS1115_device device, uint8_t reg,uint16_t* data)
 		std::cout << "Unable to open " << device.addr << "\n";
 		return ERR_NOT_FOUND;
 	}
-	char buf[2] = { 0 };
+	char buf[2];
 	ret = i2cReadI2CBlockData(fd, reg, buf, 2);
 	if (ret < 0)
 	{
@@ -45,7 +47,7 @@ int ADS1115::i2c_read(ADS1115_device device, uint8_t reg,uint16_t* data)
 	return ret;
 }
 
-int ADS1115::i2c_value(ADS1115_device device, uint16_t* data)
+int ADS1115::_i2c_value(ADS1115_device device, uint16_t* data)
 {
 	int ret = 0;
 	int fd = i2cOpen(I2C_BUS, device.addr, 0);
@@ -80,24 +82,48 @@ int ADS1115::start(ADS1115_device device)
 		throw "GPIO initialization failed";
 		return ERR_GPIO_INIT_FAILURE;
 	}
-	i2c_write(device, Lo_thresh_REG, 0x00);
-	i2c_write(device, Hi_thresh_REG, 0x8000);
+	_i2c_write(device, Lo_thresh_REG, 0x00);
+	_i2c_write(device, Hi_thresh_REG, 0x8000);
 	uint16_t config = 0;
-	i2c_read(device, CONFIG_REG, &config);
+	_i2c_read(device, CONFIG_REG, &config);
 	config &= ~config;
-	config = 1 << 15 | 1 << 14 | PGA << 9 | 1 << 3 | 1 << 2;
-	i2c_write(device,CONFIG_REG, config);
+	config = START << 15 | device.input_channel << ANALOG_INPUT_POS | device.pga << PGA_POS | COMP_POL << 3 | COMP_LAT << 2 | device.data_rate << DATA_RATE_POS;
+	_i2c_write(device,CONFIG_REG, config);
 	gpioSetMode(ALRT_PIN, PI_INPUT);
 	gpioSetISRFuncEx(ALRT_PIN, RISING_EDGE, 1000, interrupt_cb, (void*)this);
+	return 0;
 }
 
+float ADS1115::_compute_scaler(uint8_t pga)
+{
+	switch (pga) 
+	{
+		case PGA_6_144:
+			return 6.144f;
+		case PGA_4_096:
+			return 4.096f;
+		case PGA_2_048:
+			return 2.048f;
+		case PGA_1_024:
+			return 1.024f;
+		case PGA_0_512:
+			return 0.512f;
+		case PGA_0_256:
+			return 0.256f;
+	}
+	return 0;
+}
 void ADS1115::_data_ready()
 {
 	uint16_t value = 0;
-	i2c_value(device, &value);
-	newdata((float)value);
+	_i2c_value(device, &value);
+	float val = (float)value / (float)0x7fff * _compute_scaler(device.pga) * 2;
+	newdata(val);
 }
-void ADS1115::main()
+
+void ADS1115::stop()
 {
-	std::cout << "Starting application";
+	gpioSetISRFuncEx(ALRT_PIN, RISING_EDGE, -1, NULL, (void*)this);
+
+	gpioTerminate();
 }
