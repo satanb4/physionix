9b3c1a0d8de2ba738522e460a8bd638b6f7ae2df
Sayan Bandyopadhyay <2672133B@student.gla.ac.uk>
2023-04-11 21:49:50 +0000

    Updated code for emg filtering

--- src/EMGProcessing/include/emg_filter.h
+++ src/EMGProcessing/include/emg_filter.h
@@ -1,18 +1,48 @@
+#include <fftw3.h>
 #include <iostream>
 #include <vector>
+
+#ifndef EMG_FILTER_H
+#define EMG_FILTER_H
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <chrono>
 #include <thread>
+#include <condition_variable>
+#include <mutex>
 
 class EMGFilter {
+public:
+    EMGFilter(int sampleRate = 200, int windowSize = 256, int filterOrder = 2, double lowPassCutoff = 20, double highPassCutoff = 20, double threshold = 0.1);
+    double getMovement();
+    void setData(const std::vector<double>& data);
+    void start(std::vector<double>& emgData);
+    void stop();
+
 private:
-    const int sampleRate = 200; // sample rate in Hz
-    const int windowSize = 256; // window size for FFT
-    const int filterOrder = 2; // filter order
-    const double lowPassCutoff = 20; // low pass filter cutoff frequency in Hz
-    const double highPassCutoff = 20; // high pass filter cutoff frequency in Hz
-    const double threshold = 0.1; // movement detection threshold
-    std::vector<double> buffer; // circular buffer for filtering
-    std::vector<double> lowPassCoeffs; // low pass filter coefficients
-    std::vector<double> highPassCoeffs; // high pass filter coefficients
-    bool running; // flag to indicate if thread is running
-    std::thread emgThread; // thread to run EMG acquisition and filtering
+    int sampleRate;
+    int windowSize;
+    int filterOrder;
+    double lowPassCutoff;
+    double highPassCutoff;
+    double threshold;
+    std::vector<double> buffer;
+    std::vector<double> lowPassCoeffs;
+    std::vector<double> highPassCoeffs;
+    std::vector<double> emgData;
+    double movement;
+    bool running;
+    bool dataReady;
+    std::condition_variable dataCond;
+    std::mutex dataMutex;
+    std::thread emgThread;
+
+    std::vector<double> butterworthLowPassCoeffs(int order, double cutoff, int sampleRate);
+    std::vector<double> filterData(const std::vector<double>& data, const std::vector<double>& coeffs);
+    std::vector<double> calculateFFT(const std::vector<double>& data);
+    double extractMovement(const std::vector<double>& fftData, double threshold);
 };
+
+#endif
