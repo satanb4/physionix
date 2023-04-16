6e3021a83babbe11f3c60a748c13cf8c5ecac5ed
Sayan Bandyopadhyay <2672133B@student.gla.ac.uk>
2023-04-16 19:52:02 +0000

    Fix for setup script of webserver

--- src/EMGApi/website/setup.sh
+++ src/EMGApi/website/setup.sh
@@ -5,8 +5,15 @@ base_dir="$(pwd)"
 nginx_dir="/etc/nginx"
 web_dir="/var/www/html"
 
+# Install nginx
+echo 'Installing nginx...'
+apt-get install -y nginx
+echo 'Nginx installed.'
+
+
+
 # Copy the website files
 echo 'Copying website files...'
-cp $base_dir/nginx-sites-enabled-default $nginx_dir/sites-enabled/default
-cp -r $base_dir/include $base_dir/index.html $web_dir
+sudo cp $base_dir/nginx-sites-enabled-default $nginx_dir/sites-enabled/default
+sudo cp -r $base_dir/include $base_dir/index.html $web_dir
 echo 'Website files copied.'
