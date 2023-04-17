e5be527f781182d018ff05709089d3f4a08e444d
Sayan Bandyopadhyay <2672133B@student.gla.ac.uk>
2023-04-17 21:50:22 +0000

    Add documentation for ADS1115 implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file main.cpp
+ * @brief Main file for the EMGdata application
+ * @author Midhun, Sayan
+ * @version 1.0
+ * @date 2023-04-17
+ * This is the entrypoint for the EMG application. It calls the functionality from the EMGdata class.
+ * Contains the _start and _stop functions.
+ * @bug No known bugs.
+*/
+
 #include "EMGData/EMGdata.h"
 
 int main(int argc, char* argv[]) {
